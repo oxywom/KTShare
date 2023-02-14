5f58f65c52ece1ecbc43bcdae159cd34cec3cd25
oxywom <oxywom@daum.net>
2023-02-14 16:57:54 +0900

    delete
    delete

--- JpaJoinEx/src/main/java/com/example/join/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.join.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.join.entity.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-	public Team findByName(String name);
-}
