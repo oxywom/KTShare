11001b134de6e057670cb98d28714337ab99e868
oxywom <oxywom@daum.net>
2023-02-22 08:37:36 +0900

    kt spring delete

--- JpaJoinEx/src/main/java/com/example/join/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.join.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.join.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-
-}
